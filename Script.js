f30b5241d6c99be795ed2707b703dedfff29c21b
Movin Shedy <movinshedy@gmail.com>
2024-09-15 17:46:53 +0300

    Javascript assignment

---
+++ Script.js
@@ -0,0 +1,23 @@
+
+// Javascript code that will input the sum of two number
+const sum = (a, b)=>{
+    return a + b
+}
+  console.log(sum(4,8))  
+
+  // javascript code that will convert minutes to seconds
+convert =(minutes) =>{ return 60*minutes;}
+
+// javascript code that returns the next number from the integer passed 
+function addition (num){
+    return num +1;
+    
+}
+
+//javascript code for getting area of triangle
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+var s = (side1 + side2 + side3) / 2;
+var Area = Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)))
+console.log(area);
